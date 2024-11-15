9e8a3246f642c132185c814372130709574cc2b8
fatimacm <19690338@tecvalles.mx>
2024-11-15 17:23:21 -0600

    Migration to Vite

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { createPinia } from 'pinia'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
+import { loadFonts } from './plugins/webfontloader';
+import vuetify from './plugins/vuetify'; 
 
-loadFonts()
+loadFonts();
 
 const app = createApp(App);
-
 const pinia = createPinia();
 
 app
   .use(router)
   .use(pinia)
   .use(vuetify)
-  .mount('#app')
+  .mount('#app');
