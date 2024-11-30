0a299b97735b7f45f36e2dcd8222a5fb777ae961
fatimacm <19690338@tecvalles.mx>
2024-11-29 19:43:25 -0600

    readme added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,81 +1,79 @@
 <template>
-    <!-- main -->
-       <!--  <v-main 
-        class="d-flex-column justify-center align-center pa-5"
-        style="min-height: 300px;"
-        > -->
-            <v-container>
-                <div class="d-flex flex-column">
-                <div class="d-flex">
-                    <h1>Home</h1>
-                    <v-spacer></v-spacer>
-                    <!-- barra busqueda -->
-                    <SearchBar @update:search="handleSearch"  />             
-                </div>
-                <v-divider ></v-divider>
-                <div class="mt-5 pa-5">
-                    <!--en esta funcion se rellena el cuerpo del home con los datos del usuario-->
-                    <CardVariant 
-                    v-for="(object, id) in objects"
-                    :key="id"
-                    :title="object.title"
-                    :description="object.description"
-                    :autor="object.autor"
-                    >
-                    <!-- boton agregar -->
-                    <template #button>
-                        <button 
-                        @click="handleAdd(object)"
-                        :class="  object.added ? 'btn-added' : 'btn-add' "
-                        :disabled="object.added"
-                        >
-                        {{ object.added ? 'Added' : 'Add' }}    
-                        </button>
-                    </template>
-                    </CardVariant>
-                </div>           
-            </div>        
-            </v-container>           
-        <!-- </v-main> -->
+  <!-- main -->
+  <v-main
+    class="d-flex-column justify-center align-center pa-5"
+    style="min-height: 300px"
+  >
+    <v-container>
+      <div class="d-flex flex-column">
+        <div class="d-flex">
+          <h1>Home</h1>
+          <v-spacer></v-spacer>
+          <!-- barra busqueda -->
+          <SearchBar @update:search="handleSearch" />
+        </div>
+        <v-divider></v-divider>
+        <div class="mt-5 pa-5">
+          <CardVariant
+            v-for="(object, id) in objects"
+            :key="id"
+            :title="object.title"
+            :description="object.description"
+            :autor="object.autor"
+          >
+            <!-- boton agregar -->
+            <template #button>
+              <button
+                @click="handleAdd(object)"
+                :class="object.added ? 'btn-added' : 'btn-add'"
+                :disabled="object.added"
+              >
+                {{ object.added ? "Added" : "Add" }}
+              </button>
+            </template>
+          </CardVariant>
+        </div>
+      </div>
+    </v-container>
+  </v-main>
 </template>
 
 <script setup>
-import SearchBar from '@/components/SearchBar.vue';
-import CardVariant from '@/components/CardVariant.vue';
-import { ref } from 'vue';
+import SearchBar from "@/components/SearchBar.vue";
+import CardVariant from "@/components/CardVariant.vue";
+import { ref } from "vue";
 
-const currentSearch = ref('');
+const currentSearch = ref("");
 const handleSearch = (search) => {
-  currentSearch.value = search; 
+  currentSearch.value = search;
 };
 
 const objects = ref([
-    {
-        id: 1,
-        title: 'Object 1',
-        description: 'Description',
-        autor: 'User',
-        added: false
-    },
-    {
-        id: 2,
-        title: 'Object 2',
-        description: 'Description',
-        autor: 'User',
-        added: false
-    },
+  {
+    id: 1,
+    title: "Object 1",
+    description: "Description",
+    autor: "User",
+    added: false,
+  },
+  {
+    id: 2,
+    title: "Object 2",
+    description: "Description",
+    autor: "User",
+    added: false,
+  },
 ]);
 
 //funcion boton para agregar
 const handleAdd = (object) => {
-    object.added = !object.added
-}
-
+  object.added = !object.added;
+};
 </script>
 
 <style scoped>
 .btn-add {
-  background-color: #11212D;
+  background-color: #11212d;
   color: white;
   padding: 5px 20px;
   border: none;
@@ -89,7 +87,7 @@ const handleAdd = (object) => {
 }
 
 .btn-added {
-  background-color: #57778F;
+  background-color: #57778f;
   color: white;
   padding: 5px 20px;
   border: none;
@@ -97,6 +95,4 @@ const handleAdd = (object) => {
   cursor: pointer;
   margin-left: 15px;
 }
-
 </style>
-
